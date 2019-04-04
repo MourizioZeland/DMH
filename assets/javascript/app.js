3ef36b7e1a4c9136c85f7f9a632c677341024f6b
Mourizio Zeland <zelandmourizio@gmail.com>
2019-04-03 20:57:53 -0700

    dropdownlistadded

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,26 +3,68 @@ $(document).ready(function () {
   
   
   // var of location banks
-var hospital = ["Banner Good Samaritan Hospital " , "St. Joseph's Hospital", "Phoenix Memorial Hospital", "Arizona Heart Hospital", "MIHS", "Honorhealth John C. Lincoln", "Phoenix Indian Hosptial", "Banner Estrella", "Honorhealth Deer Valley", "Abrazo Central Campus", "Valley Hospital", "St. Luke's Hosptial"];
+var hospital = ["Banner Good Samaritan Hospital " , "St. Joseph\'s Hospital", "Phoenix Memorial Hospital", "Arizona Heart Hospital", "MIHS", "Honorhealth John C. Lincoln", "Phoenix Indian Hosptial", "Banner Estrella", "Honorhealth Deer Valley", "Abrazo Central Campus", "Valley Hospital", "St. Luke\'s Hosptial"];
 var shelter = ["Homeward Bound", "Church on the Street ", "The Respite Shelter for Homeless Men", "Homebase Youth Services" , "Arizona Coalition to End Homelessness" , "Andre House", "Vista Colina Emergency Family Shelter", "Justa Center", "Homeless ID Project","Lodestar Day Resource Center", "Brian Garcia Welcome Center", "Central AZ Shelter Services","House Of Refuge Sunnyslope","Gift of Mary Womens Shelter","Phoenix Rescue Mission","Tumbleweed Phoenix Resource Center", "Terros Safe Haven Shelter", "Kaiser Family Crisis Shelter", "Elim House Shelter", "UMOM Family Shelter", "Family Promise of Greater Phoenix" ,"La Mesita Family Homeless Shelter", "East Valley Men's Center (EVMC) Shelter", "Save the Family", "East Valley Women's Shelter"];
-var foodBank = ["St. Mary's Food Bank","Cultral Cup Food Bank","Valley Christian Centers","St Stephen's Episcopal Church","Tanner Chapel A.M.E. Church","St Gregory's - St Vincent de Paul","South Mountain Community College","Rio Vista Center","Operation Care-Valley Heights","Open Door Fellowship Church","Mount of Olives Lutheran Church Food Pantry","Neighborhood Ministries Inc.","Manzanita Senior Center", "Living Streams Church Food Pantry", "Life Bridge Resource Center","Joshua Tree Feeding Program","Highways and Hedges Ministries", "Friendly House", "First Southern Baptist Church Benevolence Center","First Pentecostal Church Community Center","FIBCO Family Services", "Desert West Senior Services", "Desert Christian Fellowship" ,"Deer Valley Senior Center","Circle of Life Development","Cultural Cup Food Bank", "Church on Fillmore", "Central AZ Shelter Services (CASS)", "Carl Hayden Veterans Hospital", "Black Family and Child Services", "Bethel Lutheran Church", "All Tribes Assembly of God Church", "Agape Network", "ICM Food and Clothing Bank", "Gateway Church of God", "Desert Mission Food Bank","Covenant of Grace Ministries" ,"Phoenix Rescue Mission", "Northminster Food Bank", "Mom's Pantry"]
+var food = ["St. Mary\'s Food Bank","Cultral Cup Food Bank","Valley Christian Centers","St Stephen\'s Episcopal Church","Tanner Chapel A.M.E. Church","St Gregory\'s - St Vincent de Paul","South Mountain Community College","Rio Vista Center","Operation Care-Valley Heights","Open Door Fellowship Church","Mount of Olives Lutheran Church Food Pantry","Neighborhood Ministries Inc.","Manzanita Senior Center", "Living Streams Church Food Pantry", "Life Bridge Resource Center","Joshua Tree Feeding Program","Highways and Hedges Ministries", "Friendly House", "First Southern Baptist Church Benevolence Center","First Pentecostal Church Community Center","FIBCO Family Services", "Desert West Senior Services", "Desert Christian Fellowship" ,"Deer Valley Senior Center","Circle of Life Development","Cultural Cup Food Bank", "Church on Fillmore", "Central AZ Shelter Services (CASS)", "Carl Hayden Veterans Hospital", "Black Family and Child Services", "Bethel Lutheran Church", "All Tribes Assembly of God Church", "Agape Network", "ICM Food and Clothing Bank", "Gateway Church of God", "Desert Mission Food Bank","Covenant of Grace Ministries" ,"Phoenix Rescue Mission", "Northminster Food Bank", "Mom\'s Pantry"]
+
+
 loopMe();
 // loops for arrays
 function loopMe() {
-
-        for (var i = 0; i < hospital.length; i++); {
-            console.log(hospital);
-
+        var k = 0;
+        var j = 0;
+        var i = 0;
+        while (i < food.length) {
+            var placer = document.createElement("a");
+            placer.classList.add("dropdown-item");
+            placer.setAttribute("href","#");
+            placer.textContent = food[i];
+            var holder = document.getElementById("foodmenu");
+            holder.appendChild(placer);
+            i++;
         }
-        for (var i = 0; i < shelter.length; i++); {
-            console.log(shelter);
-
+        
+        while (j < hospital.length) {
+            var placer = document.createElement("a");
+            placer.classList.add("dropdown-item");
+            placer.setAttribute("href","#");
+            placer.textContent = hospital[j];
+            var holder = document.getElementById("hospitalsmenu");
+            holder.appendChild(placer);
+            j++;
         }
-        for (var i = 0; i < foodBank.length; i++); {
-            console.log(foodBank);
+        while (k < shelter.length) {
+            var placer = document.createElement("a");
+            placer.classList.add("dropdown-item");
+            placer.setAttribute("href","#");
+            placer.textContent = shelter[k];
+            var holder = document.getElementById("sheltermenu");
+            holder.appendChild(placer);
+            k++;
+        }
+
+        // for (var i = 0; i < hospital.length; i++); {
+        //     console.log('i',i);
+        //     var placer = document.createElement("a");
+        //     placer.classList.add("dropdown-item");
+        //     placer.setAttribute("href","#");
+        //     placer.textContent=hospital;
+        //     var holder = document.getElementById("foodmenu");
+        //     holder.appendChild(placer);
+        //     console.log(placer);
+        //     console.log(holder);
+        //     console.log(hospital,i);
+            
+        // }
+       // for (var i = 0; i < shelter.length; i++); {
+          
+
+       // }
+      //  for (var i = 0; i < foodBank.length; i++); {
+       
 
         
-        }
+      //  }
 
 }
 
@@ -101,9 +143,9 @@ function loopMe() {
         database.ref().on("child_added", function (childSnapshot) {
 
             coordLat = parseFloat(childSnapshot.val().Coordinates_Latitude);
-            console.log(coordLat);
+           
             coordLong = parseFloat(childSnapshot.val().Coordinates_Longitude);
-            console.log(coordLong);
+           
         });
 
         var userLoc = {
